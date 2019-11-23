61c82166eaac7e9699e2b6185ba889e5faa0bdb2
vinay <vinay@gmail.com>
2019-11-23 15:30:40 +0530

    greatings method added

--- Project/src/main/java/demo/com/DemoClass.java
+++ Project/src/main/java/demo/com/DemoClass.java
@@ -1,7 +1,13 @@
 package demo.com;
 
 public class DemoClass {
-public static void main(String[] args) {
-	
-}
+  
+	public static void greatings() {
+		
+		System.out.println("hi git ");
+	}
+ 
+	public static void main(String[] args) {
+
+	}
 }
