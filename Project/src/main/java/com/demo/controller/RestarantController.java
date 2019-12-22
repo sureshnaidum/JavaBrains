2ff7eab7966eff60efc9165f8758b80fe5e01fe2
suresh <naidu@gmail.com>
2019-12-22 11:53:31 +0530

    Revert "adding demo method"
    This reverts commit e498729537fcd57a27338a04d75a86285f11410f.

--- Project/src/main/java/com/demo/controller/RestarantController.java
+++ Project/src/main/java/com/demo/controller/RestarantController.java
@@ -5,9 +5,4 @@ public class RestarantController {
 	public static void main(String[] args) {
 		System.out.println("controller is ready");
 	}
-	
-	private void demo() {
-		// TODO Auto-generated method stub
-
-	}
 }
